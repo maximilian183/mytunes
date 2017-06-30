800fa8497a582f76126f76e2d677901224e9d6bc
maximilian183 <max.kwok@gmail.com>
2017-06-30 10:33:51 -0700

    PlayerView plays next song when currentsong is updated in AppModel

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,10 +4,25 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add remove', function(){
+      if ( this.size() === 1 ) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended dequeue', function() {
+      // Removes the first song in the Song Queue when any song is being played.  Assumption is that the first song in queue is played
+      this.remove(this.at(0));
+    }, this);
+
+
   },
 
   enqueue: function(song) {
     this.push(song);
-  }
+  },
 
+  playFirst: function() {
+    this.at(0).play();
+  }
 });
